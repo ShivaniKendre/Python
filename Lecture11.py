3b9cc6d10a35771f7d3b4d3333792d1b80e05c7e
Shivani Kendre <shivanikendre812@gmail.com>
2025-12-10 22:05:14 +0530

    for loop

---
+++ Lecture11.py
@@ -0,0 +1,8 @@
+for i in range(1,6):
+    print("Hello World")    
+# mutable,sequence
+mystring="Hello "   
+mystring=mystring+"World"
+print("mystring=",mystring)
+print(mystring[0])
+print(mystring[1])
